b540381a2da297e3e6a5f79238b1025d17197530
Stephen Moore <stermmoore@gmail.com>
2022-08-13 18:02:13 +0100

    get cosmos db connection string

--- CosmosDB/Program.cs
+++ CosmosDB/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets<Program>()
+    .Build();
+
+var connectionString = config.GetSection("CosmosConnectionString")?.Value;
+
+var cosmosClient = new CosmosClient(connectionString);
