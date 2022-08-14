7b73e66afa0f9680936f155a44794e35f6f6c500
Stephen Moore <stermmoore@gmail.com>
2022-08-14 17:22:29 +0100

    create container

--- CosmosDB/Program.cs
+++ CosmosDB/Program.cs
@@ -10,8 +10,28 @@ var connectionString = config.GetSection("CosmosConnectionString")?.Value;
 
 await CreateDB("sample-db");
 
+await CreateContainer("sample-db", "sample-container", "/location");
+
+
+//Write Item
+
+//Read Item
+
+//Read Items
+
+//Change Item
+
 async Task CreateDB(string dbName)
 {
     var cosmosClient = new CosmosClient(connectionString);
     await cosmosClient.CreateDatabaseIfNotExistsAsync(dbName);
+}
+
+async Task CreateContainer(string databaseName, string containerName, string partitionKey)
+{
+    var cosmosClient = new CosmosClient(connectionString);
+
+    var database = cosmosClient.GetDatabase(databaseName);
+
+    await database.CreateContainerIfNotExistsAsync(containerName, partitionKey);
 }
