a1f9251e6ceaf03f95e19ef4b60beff40e3c6601
Stephen Moore <stermmoore@gmail.com>
2022-08-16 18:30:39 +0100

    added blob storage project and deploy

---
+++ BlobStorage/deploy.ps1
@@ -0,0 +1,3 @@
+﻿Connect-AzAccount #Will prompt user to login to Azure
+
+New-AzResourceGroupDeployment -ResourceGroupName "rg-azure-samples" -TemplateFile "deployment.json" -TemplateParameterFile "parameters.json"
