32f7fa56a279adad15b81677f471fd1898b95e7d
Stephen Moore <stermmoore@gmail.com>
2022-08-16 18:40:51 +0100

    create a blob container

--- BlobStorage/Program.cs
+++ BlobStorage/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets<Program>()
+    .Build();
+
+var connectionString = config.GetSection("BlobConnectionString")?.Value;
+
+
+var blobServiceClient = new BlobServiceClient(connectionString);
+
+await blobServiceClient.CreateBlobContainerAsync("test-blob");
