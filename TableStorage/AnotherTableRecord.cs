4717561a548d43f85b3f5892e72de3588cc6e18f
Stephen Moore <stermmoore@gmail.com>
2022-08-20 10:38:05 +0100

    read a record with different properties from the same table

---
+++ TableStorage/AnotherTableRecord.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableStorage
+{
+    internal class AnotherTableRecord : ITableEntity
+    {
+        public string PartitionKey { get; set; } = nameof(AnotherTableRecord);
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+        public string TypeName { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
--- TableStorage/Program.cs
+++ TableStorage/Program.cs
@@ -25,9 +25,18 @@ await tableClient.AddEntityAsync<TableRecord>(new TableRecord { Name = "Dave" })
 
 
 //Query the table
-var allRecords = tableClient.QueryAsync<TableRecord>(r => r.PartitionKey == nameof(TableRecord));
+var tableRecords = tableClient.QueryAsync<TableRecord>(r => r.PartitionKey == nameof(TableRecord));
 
-await foreach(var record in allRecords)
+await foreach(var record in tableRecords)
 {
     Console.WriteLine($"{record.Timestamp} - {record.Name}");
+}
+
+
+//Query for records with different properties
+var differentRecords = tableClient.QueryAsync<AnotherTableRecord>(r => r.PartitionKey == nameof(AnotherTableRecord));
+
+await foreach (var record in differentRecords)
+{
+    Console.WriteLine($"{record.Timestamp} - {record.TypeName}");
 }
