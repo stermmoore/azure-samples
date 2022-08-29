c23bd7d0560a1cd91fe9b9198cc2bae6ed89ffb9
Stephen Moore <stermmoore@gmail.com>
2022-08-29 10:30:04 +0100

    added full web app deployment from files

--- WebApp/deploy.ps1
+++ WebApp/deploy.ps1
@@ -1,7 +1,17 @@
 ﻿dotnet publish --configuration Release --output .\bin\publish
 
-Compress-Archive .\bin\publish\* .\bin\publish\web.zip
+Compress-Archive .\bin\publish\* .\bin\publish\web.zip -Force
 
 Connect-AzAccount
 
-Publish-AzWebApp -ResourceGroupName rg-azure-samples -Name 34534wesdr23 -ArchivePath .\bin\publish\web.zip
+$appName = "54534wesdr23"
+
+$params = @{
+
+	webAppName = $appName
+
+}
+
+New-AzResourceGroupDeployment -ResourceGroupName "rg-azure-samples" -TemplateFile "deployment.json" -TemplateParameterObject $params
+
+Publish-AzWebApp -ResourceGroupName rg-azure-samples -Name $appName -ArchivePath .\bin\publish\web.zip
